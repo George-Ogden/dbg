0c6d484c5673c422638e2a5712177fc42cca43b0
George Ogden <george-ogden@outlook.com>
2025-07-19 19:52:30 +0100

    Add same line edge case

---
+++ test_samples/same_line.py
@@ -0,0 +1,6 @@
+from debug import dbg
+
+x = 3
+y = 4
+
+print(dbg(x) + dbg(y))
--- test_suite.py
+++ test_suite.py
@@ -1,4 +1,5 @@
 import importlib
+import textwrap
 
 from _pytest.capture import CaptureFixture
 import pytest
@@ -13,11 +14,22 @@ MODULE = "test_samples"
         ("variable", "17", "[variable.py:4] x = 17"),
         ("two_line", "-32", "[two_line.py:6] x * y = -32"),
         ("three_line", "14\n14", "[three_line.py:3] (y := (10 + 4)) = 14"),
+        (
+            "same_line",
+            "7",
+            """
+            [same_line.py:6] x = 3
+            [same_line.py:6] y = 4
+            """,
+        ),
     ],
 )
 def test_samples(name: str, expected_out: str, expected_err, capsys: CaptureFixture) -> None:
     module = f"{MODULE}.{name}"
     importlib.import_module(module)
+
+    expected_out = textwrap.dedent(expected_out).strip()
+    expected_err = textwrap.dedent(expected_err).strip()
 
     out, err = capsys.readouterr()
     assert out.strip() == expected_out
